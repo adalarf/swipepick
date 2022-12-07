d4f3456195eaa4fbf723181c08f18b825c7dd870
notanumer <kolchin-03@inbox.ru>
2022-12-07 23:20:48 +0500

    Создание базовых сущностей и миграций

---
+++ Swipepick.Backend/DAL/AppDBContext/DalSettings.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class DalSetting
+{
+    public string ConnectionString { get; }
+
+    public DalSetting(IConfiguration configuration)
+    {
+        ConnectionString = configuration.GetSection("Dal").GetValue<string>("ConnectionString");
+    }
+}
---
+++ Swipepick.Backend/DAL/Entities/QuestionDal.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Entities
+{
+    public class QuestionDal
+    {
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public TestDal Test { get; set; }
+        public string Question { get; set; }
+        public AnswerDal Answers { get; set; }
+    }
+}
