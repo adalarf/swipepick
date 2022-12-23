93337fa3f5aa9ed105e180effc450ac4ef6651bd
notanumer <kolchin-03@inbox.ru>
2022-12-23 21:15:17 +0500

    обновил базу данных и внес данные  пользователе и его тесте

--- Swipepick.Backend/SwipepickServer/Migrations/20221223160136_New.Designer.cs
+++ Swipepick.Backend/SwipepickServer/Migrations/20221223160136_New.Designer.cs
@@ -2,6 +2,7 @@
 using DAL.AppDBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace SwipepickServer.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20221223160136_New")]
+    partial class New
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
