6cbd05d42d3bca330c06c290ad3921c7bbb6293e
adalarf <kirilsloboda@gmail.com>
2023-04-06 22:16:52 +0500

    Верстка с flexbox

--- Swipepick.Frontend/pages/profile.js
+++ Swipepick.Frontend/pages/profile.js
@@ -22,38 +22,53 @@ const Profile = () => {
       <div className={styles.logo}><Logo href="/" src={logo2}/></div>
       {/* <button onClick={() => dispatch(logout())}>ВЫЙТИ</button>
       {isAuth ? <p>ok</p> : <p>not</p>} */}
-      <div className={styles.menu}></div>
-      <Image src = {user} className = {styles.user}></Image>
+      {/* <div className={styles.menu}></div>
+      <Image src = {user} className = {styles.user}></Image> */}
+      <Image src = {user} className = {styles.user1}></Image>
+
       <div className={styles.tests_and_polls}>
         <div className={styles.tests}>тесты</div>
         <div className={styles.polls}>опросы</div>
       </div>
-      <div className={styles.plus_block}>
-        <div className={styles.plus}>+</div>
+
+      <div className={styles.container}>
+        <div className={styles.answer_types}>
+          <div className={styles.answer_type_container}>
+            <div className={styles.answer_type_selected}>новые</div>
+            <div className={styles.answer_type}>старые</div>
+            <div className={styles.answer_type}>верные</div>
+            <div className={styles.answer_type}>ошибки</div>
+          </div>
+        </div>
+        <div className={styles.plus_block}>
+          <div className={styles.plus}>+</div>
+        </div>
       </div>
-      <div className={styles.answer_types}>
-        <div className={styles.answer_type}>верные</div>
-        <div className={styles.answer_type}>ошибки</div>
-        <div className={styles.answer_type} id = {styles.new}>новые</div>
-        <div className={styles.answer_type}>старые</div>
-      </div>
+
       <div className={styles.answers}>
-        <div className={styles.answer}>
-          <div className={styles.name}>Евгений Онегин</div>
-          <div className={styles.code}>S8W5</div>
-          <div className={styles.date}>10.03.2023</div>
-          <Image src={settings} className={styles.settings}></Image>
-          <Image src={delete_img} className={styles.delete}></Image>
+
+        <div className={styles.answer}> 
+          <div className={styles.answer_container}>
+            <div className={styles.name}>Евгений Онегин</div>
+            <div className={styles.code}>S8W5</div>
+            <div className={styles.date}>10.03.2023</div>
+            <Image src={settings} className={styles.settings}></Image>
+            <Image src={delete_img} className={styles.delete}></Image>
+          </div>
           <div className={styles.columns}></div>
         </div>
+
         <div className={styles.answer}>
-        <div className={styles.name}>Квадратные уравнения</div>
-          <div className={styles.code}>3000</div>
-          <div className={styles.date}>28.02.2023</div>
-          <Image src={settings} className={styles.settings}></Image>
-          <Image src={delete_img} className={styles.delete}></Image>
+          <div className={styles.answer_container}>
+            <div className={styles.name}>Квадратные уравнения</div>
+            <div className={styles.code}>3000</div>
+            <div className={styles.date}>28.02.2023</div>
+            <Image src={settings} className={styles.settings}></Image>
+            <Image src={delete_img} className={styles.delete}></Image>
+          </div>
           <div className={styles.columns}></div>
         </div>
+
       </div>
     </Layout>
   )
